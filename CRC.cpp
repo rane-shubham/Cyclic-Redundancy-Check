b48ffaf33c6732471962db669a6213e44b9e8dd3
rane-shubham <raneshubh123@gmail.com>
2020-10-14 22:57:44 +0530

    Modified using Class

--- CRC.cpp
+++ CRC.cpp
@@ -5,92 +5,119 @@ Name: Shubham D. Rane
 #include <iostream>
 using namespace std;
 
-void divide(int n, int g, int temp[10], int gen[10])
+class CRC
 {
-    for(int i=0; i<n; i++)
+public:
+    int nf, ng, frame[20], gen[10], temp[20], b;
+    char a;
+
+    int* divide(int n, int g, int temp[10], int gen[10])
     {
-        if(gen[0]==temp[i])
+        for(int i=0; i<n; i++)
         {
-            for(int j=0, k=i; j<g+1; j++, k++)
+            if(gen[0]==temp[i])
             {
-                if(temp[k]^gen[j]==1)
-                    temp[k]=1;
-                else
-                    temp[k]=0;
+                for(int j=0, k=i; j<g+1; j++, k++)
+                {
+                    if(temp[k]^gen[j]==1)
+                        temp[k]=1;
+                    else
+                        temp[k]=0;
+                }
             }
         }
+        return temp;
     }
-}
+
+    void input()
+    {
+        cout<<"Enter length of your frame:";
+        cin>>nf;
+        cout<<"Enter your frame:";
+        for(int i=0; i<nf; i++)
+        {
+            cin>>frame[i];
+            temp[i]=frame[i];
+        }
+
+        cout<<"Enter length of your generator:";
+        cin>>ng;
+        cout<<"Enter your generator:";
+        for(int i=0; i<ng; i++)
+        {
+            cin>>gen[i];
+        }
+
+        ng--;
+
+        for(int i=0; i<ng; i++)
+        {
+            temp[nf+i]=0;
+        }
+    }
+
+    void sender_side()
+    {
+        int* sender;
+        sender = divide(nf,ng,temp,gen);
+        cout<<endl<<"-----Senders Side-----\n"<<"CRC:";
+
+        for(int i=0; i<ng; i++)
+        {
+            frame[nf+i]=sender[nf+i];
+            cout<<sender[nf+i]<<' ';
+        }
+        cout<<endl<<"Transmitted frame:";
+
+        for(int i=0; i<nf+ng; i++)
+            cout<<frame[i]<<' ';
+
+        cout<<endl;
+    }
+
+    int receiver_side()
+    {
+        int* receiver;
+        cout<<"\n-----Receivers Side-----\n"<<"Received message:";
+        for(int i=0; i<nf+ng; i++)
+            cout<<frame[i]<<' ';
+        cout<<endl;
+
+        cout<<"Do you want to change any bit of the received message?(y/n)"<<endl;
+        cin>>a;
+
+        if(a=='y')
+        {
+            cout<<"Enter which bit you want to change(from 0-"<<nf+ng<<")-";
+            cin>>b;
+            if(frame[b]==1)
+                frame[b]=0;
+            else
+                frame[b]=1;
+        }
+
+        receiver = divide(nf,ng,frame,gen);
+
+        cout<<"Error:";
+        for(int i=0; i<nf+ng; i++)
+        {
+            if(receiver[i]!=0)
+            {
+                cout<<"Error Detected!!"<<endl;
+                return 0;
+            }
+        }
+        cout<<"No error detected!"<<endl;
+    }
+};
+
 
 int main()
 {
-    int nf, ng, frame[20], gen[10], temp[20], b;
-    char a;
-    cout<<"Enter length of your frame:";
-    cin>>nf;
-    cout<<"Enter your frame:";
-    for(int i=0; i<nf; i++)
-    {
-        cin>>frame[i];
-        temp[i]=frame[i];
-    }
-    cout<<"Enter length of your generator:";
-    cin>>ng;
-    cout<<"Enter your generator:";
-    for(int i=0; i<ng; i++)
-    {
-        cin>>gen[i];
-    }
-    ng--;
-    for(int i=0; i<ng; i++)
-    {
-        temp[nf+i]=0;
-    }
+    CRC o;
+    o.input();
+    o.sender_side();
+    o.receiver_side();
 
-    divide(nf,ng,temp,gen);
-    cout<<endl<<"-----Senders Side-----\n"<<"CRC:";
-
-    for(int i=0; i<ng; i++)
-    {
-        frame[nf+i]=temp[nf+i];
-        cout<<temp[nf+i]<<' ';
-    }
-    cout<<endl<<"Transmitted frame:";
-
-    for(int i=0; i<nf+ng; i++)
-        cout<<frame[i]<<' ';
-
-    cout<<endl;
-
-    cout<<"\n-----Receivers Side-----\n"<<"Received message:";
-    for(int i=0; i<nf+ng; i++)
-        cout<<frame[i]<<' ';
-    cout<<endl;
-
-    cout<<"Do you want to change any bit of the received message?(y/n)"<<endl;
-    cin>>a;
-
-    if(a=='y')
-    {
-        cout<<"Enter which bit you want to change(from 0-"<<nf+ng<<")-";
-        cin>>b;
-        if(frame[b]==1)
-            frame[b]=0;
-        else
-            frame[b]=1;
-    }
-
-    divide(nf,ng,frame,gen);
-
-    cout<<"Error:";
-    for(int i=0; i<nf+ng; i++)
-    {
-        if(frame[i]!=0)
-        {
-            cout<<"Error Detected!!"<<endl;
-            return 0;
-        }
-    }
-    cout<<"No error detected!"<<endl;
     return 0;
 }
